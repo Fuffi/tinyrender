e9836f166bde326f5a09a9bc9f66a99c0a351f71
Federico <ufinii@gmail.com>
2016-03-19 16:48:33 +0000

    Bresenham first pass

--- main.cpp
+++ main.cpp
@@ -3,11 +3,31 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+void line(const int x0, const int y0, const int x1, const int y1, TGAImage& image, const TGAColor& color) {
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int d_error = (2 * dy) - dx;
+
+	int y = y0;
+
+	for (int x = x0; x <= x1; ++x) {
+		if (d_error > 0) {
+			++y;
+			d_error -= 2 * dx;
+		}
+
+		image.set(x, y, color);
+		d_error += 2 * dy;
+	}
+}
+
 int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red);
+	TGAImage image(500, 500, TGAImage::RGB);
+
+	line(100, 100, 250, 130, image, red);
+	image.set(99, 100, white);
+	image.set(251, 130, white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
 }
-
